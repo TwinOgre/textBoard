486c21fe6c3d69ba171db2104fd5aa70066f9c37
TwinOgre <glad2you@naver.com>
2023-12-01 18:51:00 +0900

    wiseWord02

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +11,8 @@ public class App {
     }
     void run(){
         System.out.println("== 명언 앱 ==");
+        int lastId = 1;
+        List<WiseSaying> wiseSayingList = new ArrayList<>();
         while (true){
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
@@ -20,8 +24,18 @@ public class App {
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
 
-                System.out.println(content);
-                System.out.println(author);
+                WiseSaying wiseSaying = new WiseSaying(lastId,content,author);
+
+                wiseSayingList.add(wiseSaying);
+
+                System.out.println( lastId + "번 명언이 등록되었습니다.");
+                lastId++;
+            } else if(command.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("---------------------");
+                for(WiseSaying wiseSaying : wiseSayingList){
+                    System.out.println(wiseSaying.getId() + " / "+wiseSaying.getAuthor() + " / " + wiseSaying.getContent());
+                }
             }
         }
 
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    WiseSaying(int id, String content, String author){
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
