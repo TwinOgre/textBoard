ff880547cb10062fa71d4a6ead9ae3fb9809fe36
TwinOgre <glad2you@naver.com>
2023-12-01 18:19:23 +0900

    test02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+
+
+        List<Integer> arrList = new ArrayList<>();
+        arrList.add(10);
+        System.out.println(arrList.get(0));
     }
 }
 
