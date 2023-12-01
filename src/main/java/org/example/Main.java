8e9c1cd4370603e55c567dea29b2c526d9b606c4
TwinOgre <glad2you@naver.com>
2023-12-01 18:13:27 +0900

    01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
-        int a = 0;
-        int b = 0;
-        int c = 0;
     }
 }
-class Car {
-    String car;
-}
+
